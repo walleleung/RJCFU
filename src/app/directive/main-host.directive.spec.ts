a9cf69600be0b9e0d9ca4912357366ab2e2d6ea1
DCH <dch@rjh.com.cn>
2019-01-22 11:00:36 +0800

    移除spec

--- src/app/directive/main-host.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MainHostDirective } from './main-host.directive';
-
-describe('MainHostDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MainHostDirective();
-    expect(directive).toBeTruthy();
-  });
-});
