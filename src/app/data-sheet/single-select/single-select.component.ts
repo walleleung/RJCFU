4e8babe4c93476d7742ff9a1d2f5c2633628e18d
DCH <dch@rjh.com.cn>
2019-01-30 09:51:01 +0800

    update package

--- src/app/data-sheet/single-select/single-select.component.ts
+++ src/app/data-sheet/single-select/single-select.component.ts
@@ -1,25 +1,35 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {DesignComponentItem} from '../../model/design-component-item';
+import {DesignComponentBase} from '../../model/design-component-base';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-single-select',
   templateUrl: './single-select.component.html',
   styleUrls: ['./single-select.component.scss']
 })
-export class SingleSelectComponent implements OnInit {
+export class SingleSelectComponent implements OnInit, DesignComponentBase {
   @Output()
   edit: EventEmitter<DesignComponentItem> = new EventEmitter();
+
+  designForm: FormGroup;
 
   @Input()
   model: DesignComponentItem;
 
-  constructor() {
+  constructor(private _formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.designForm = this._formBuilder.group([
+    ]);
   }
 
   editComponent() {
     this.edit.emit(this.model);
+  }
+
+  save(): boolean {
+    return false;
   }
 }
